c299b8e1b2b9d97ab492c40cd462c980df03a30e
sujay <sujaybawaskar@gmail.com>
2018-03-17 23:02:51 +0530

    Hello World program

---
+++ core-java/src/main/java/edu/practice/HelloWorld.java
@@ -0,0 +1,15 @@
+package edu.practice;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @author Sujay Bawaskar
+ * @version 1.0
+ * @since 17-Mar-2018
+ */
+public class HelloWorld {
+    public static void main( String[] args ) {
+        System.out.println("Hello World" );
+    }
+}
